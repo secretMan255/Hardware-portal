bf6f7bb6a9ae9c28c274d5f234b0fdf2e1fc67a1
yiliang <yapyiliangww321@gmail.com>
2025-01-20 20:21:22 +0800

    optimize create account dialog

--- src/views/header/createAccount/createAccount.vue
+++ src/views/header/createAccount/createAccount.vue
@@ -1,10 +1,11 @@
 <template>
-     <v-dialog :model-value="modelValue" max-width="550" @update:model-value="updateDialog" persistent>
+     <v-dialog :model-value="modelValue" max-width="730" @update:model-value="updateDialog" persistent>
           <v-card prepend-icon="mdi-account" title="Create Account">
-               <v-card-text>
+               <v-card-text >
                     <v-row dense>
                          <v-col cols="12" md="12">
                               <v-text-field
+                                   :disabled="disable"
                                    variant="outlined"
                                    density="compact"
                                    v-model="createAccount.email"
@@ -14,6 +15,7 @@
                          </v-col>
                          <v-col cols="12" md="12">
                               <v-text-field
+                                   :disabled="disable"
                                    variant="outlined"
                                    density="compact"
                                    v-model="createAccount.name"
@@ -21,8 +23,9 @@
                                    required
                               ></v-text-field>
                          </v-col>
-                         <v-col cols="12" md="12">
+                         <v-col cols="12" md="6">
                               <v-text-field
+                                   :disabled="disable"
                                    variant="outlined"
                                    density="compact"
                                    :type="showPassword ? 'text' : 'password'" 
@@ -30,11 +33,26 @@
                                    label="Password*"
                                    required
                                    append-inner-icon="mdi-eye"
+                                   hint="Password must contain number and alphabet"
+                                   @click:append-inner="togglePasswordVisibility"
+                              ></v-text-field>
+                         </v-col>
+                         <v-col cols="12" md="6">
+                              <v-text-field
+                                   :disabled="disable"
+                                   variant="outlined"
+                                   density="compact"
+                                   :type="showPassword ? 'text' : 'password'" 
+                                   v-model="confirmPass"
+                                   label="Confirm Password*"
+                                   required
+                                   append-inner-icon="mdi-eye"
                                    @click:append-inner="togglePasswordVisibility"
                               ></v-text-field>
                          </v-col>
                          <v-col cols="12" md="12">
                               <v-text-field
+                                   :disabled="disable"
                                    prefix="+60"
                                    variant="outlined"
                                    density="compact"
@@ -45,6 +63,7 @@
                          </v-col>
                          <v-col cols="12" md="12">
                               <v-textarea
+                                   :disabled="disable"
                                    row-height="1"
                                    rows="4"
                                    label="Address*"
@@ -56,6 +75,7 @@
                          </v-col>
                          <v-col cols="12" md="6">
                               <v-text-field
+                                   :disabled="disable"
                                    variant="outlined"
                                    density="compact"
                                    v-model="createAccount.postCode"
@@ -64,13 +84,14 @@
                               ></v-text-field>
                          </v-col>
                          <v-col cols="12" md="6">
-                              <v-text-field
+                              <v-select
                                    variant="outlined"
                                    density="compact"
                                    v-model="createAccount.city"
+                                   :items="cityItem"
                                    label="City*"
-                                   required
-                              ></v-text-field>
+                                   :disabled="disable"
+                              ></v-select>
                          </v-col>
                          <v-col cols="12" md="12">
                               <v-select
@@ -83,7 +104,7 @@
                               ></v-select>
                          </v-col>
                          <v-col cols="12" md="12">
-                              <v-btn :disabled="isRequestOtpDisabled" block class="mb-5">Request OTP</v-btn>
+                              <v-btn :loading="otpLoading" :disabled="isRequestOtpDisabled || disableResuestOTP || disable" block class="mb-5" @click="requestOTP">Request OTP</v-btn>
                          </v-col>
                          <v-col cols="12" md="12">
                               <p class="d-flex justify-center mb-2 text-bold">Enter OTP</p>
@@ -98,11 +119,11 @@
                <v-divider></v-divider>
                <v-card-actions>
                     <v-spacer></v-spacer>
-                    <v-btn :disabled="disableCreate" @click="createUser">
-                         CREATE
+                    <v-btn :disabled="isCreateAccountDisabled" :loading="registerLoading" @click="createUser">
+                         REGISTER
                     </v-btn>
                     <v-btn @click="closeDialog">
-                         CANCEL
+                         CLOSE
                     </v-btn>
                </v-card-actions>
           </v-card>  
@@ -110,30 +131,42 @@
 </template>
 
 <script>
+import { CallApi } from '../../../CallApi/callApi'
+import { executeRecaptcha } from '../../../utils/utils'
+
 export default {
      props: {
           modelValue: {
                type: Boolean,
                required: true          
-          }
+          },
+          cityItem: {
+               type: Array,
+               required: true,
+          },
      },
      data() {
           return {
+               disable: false,
                showPassword: false,
                createAccount: {
                     email: '',
                     name: '',
                     password: '',
-                    phone: '',
+                    phone: null,
                     address: '',
                     postCode: '',
                     city: '',
                     country: 'Malaysia',
                     otp: ''
                },
+               confirmPass: '',
                countryItem: ['Malaysia'],
                disableCreate: true,
-               countryDisable: true
+               countryDisable: true,
+               disableResuestOTP: false,
+               otpLoading: false,
+               registerLoading: false
           }
      },
      methods: {
@@ -141,13 +174,14 @@ export default {
                this.showPassword = !this.showPassword;
           },
           closeDialog() {
+               this.clearParams()
                this.$emit("update:modelValue", false)
           },
           updateDialog(newValue) {
                this.$emit("update:modelValue", newValue)
           },
           emailValidate(email) {
-               return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+               return (email.length <= 45 && /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.com)$/.test(email))
           },
           validatePassword(pass) {
                return (
@@ -156,7 +190,8 @@ export default {
                     /[A-Z]/.test(pass) &&
                     /[a-z]/.test(pass) &&
                     /\d/.test(pass) &&
-                    /[!@#$%^&*(),.?":{}|<>]/.test(pass)
+                    /[!_@#$%^&*(),.?":{}|<>]/.test(pass) &&
+                    pass === this.confirmPass
                )
           },
           validatePhone(phone) {
@@ -179,12 +214,114 @@ export default {
                     name.length <= 45
                )
           },
-          async createUser() {
+          validateOtp(otp) {
+               return (
+                    otp.length === 6
+               )
+          },
+          async requestOTP() {
+               try {
+                    const sendOtpPayload = {
+                         email: this.createAccount.email,
+                         recaptchaToken: await executeRecaptcha('email_login')
+                    }
+                    
+                    this.otpLoading = true
+                    const res = await CallApi.sendCreateAccountOTP(sendOtpPayload)
 
+                    if (res.data) {
+                         this.$emit('update-snackbar', {
+                              show: true,
+                              message: res.data[0].errorMsg
+                         })
+                         return 
+                    }
+                    
+                    this.$emit('update-snackbar', {
+                         show: true,
+                         message: 'OTP sent successfully.'
+                    });
+                    this.disable = true
+                    this.disableResuestOTP = true
+
+                    setTimeout(() => {
+                         this.disableResuestOTP = false
+                    }, 300000)
+               } catch (err) {
+                    this.$emit('update-snackbar', {
+                         show: true,
+                         message: 'Please request OTP again after 5 min.'
+                    })
+                    this.disable = false
+               } finally {
+                    this.otpLoading = false
+               }
+          },
+          async createUser() {
+               try {
+                    this.registerLoading = true
+                    this.createAccount.recaptchaToken = await executeRecaptcha('email_login')
+                    const res = await CallApi.createAccount(this.createAccount)
+
+                    if (res?.data) {
+                         this.$emit('update-snackbar', {
+                              show: true,
+                              message: `Register failed. ${res?.data[0].errorMsg}`
+                         })     
+                         return
+                    }
+
+                    this.closeDialog()
+
+                    this.$emit('update-snackbar', {
+                         show: true,
+                         message: `Registered successfully.`
+                    })
+
+               } catch (err) {
+                    this.$emit('update-snackbar', {
+                         show: true,
+                         message: `Register failed.`
+                    })
+                    this.disable = false
+               } finally {
+                    this.registerLoading = false
+               }
+          },
+          clearParams() {
+               this.createAccount.email = ''
+               this.createAccount.password = ''
+               this.createAccount.name = ''
+               this.createAccount.phone = ''
+               this.createAccount.address = ''
+               this.createAccount.postCode = ''
+               this.createAccount.city = ''
+               // this.createAccount.country = ''
+               this.createAccount.otp = ''
+               this.disable = false
           }
      },
      computed: {
           isRequestOtpDisabled() {
+               return !(
+                    this.createAccount.email &&
+                    this.emailValidate(this.createAccount.email) &&
+                    this.createAccount.name &&
+                    this.validateName(this.createAccount.name) &&
+                    this.createAccount.password &&
+                    this.validatePassword(this.createAccount.password) 
+                    // &&
+                    // this.createAccount.phone &&
+                    // this.validatePhone(this.createAccount.phone) &&
+                    // this.createAccount.address &&
+                    // this.validateAddress(this.createAccount.address) && 
+                    // this.createAccount.postCode &&
+                    // this.validateAddress(this.createAccount.postCode) && 
+                    // this.createAccount.city &&
+                    // this.createAccount.country
+               )
+          },
+          isCreateAccountDisabled() {
                return !(
                     this.createAccount.email &&
                     this.emailValidate(this.createAccount.email) &&
@@ -198,20 +335,13 @@ export default {
                     this.validateAddress(this.createAccount.address) && 
                     this.createAccount.postCode &&
                     this.validateAddress(this.createAccount.postCode) && 
-                    this.createAccount.city
-                    // this.createAccount.country
+                    this.createAccount.city &&
+                    this.createAccount.country &&
+                    this.createAccount.otp &&
+                    this.validateOtp(this.createAccount.otp) &&
+                    this.createAccount.country
                )
-          },
-     },
-     mounted() {
-          this.createAccount.email = ''
-          this.createAccount.password = ''
-          this.createAccount.phone = ''
-          this.createAccount.address = ''
-          this.createAccount.postCode = ''
-          this.createAccount.city = ''
-          // this.createAccount.country = ''
-          this.createAccount.otp = ''
+          }
      }
 }
 </script>
