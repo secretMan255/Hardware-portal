e279af53ddadbfc0b16ede93b3e3b8653a5c6331
yiliang <yapyiliangww321@gmail.com>
2025-01-23 20:51:59 +0800

    handle mobile view

--- src/views/header/header.vue
+++ src/views/header/header.vue
@@ -1,13 +1,15 @@
 <template>
      <v-app-bar app dense>
-               <div class="app-bar-content">
+                <div class="app-bar-content">
                     <p class="company-name" @click="navigationTo('/')">Company Name</p>
                     <p class="nav-link" @click="navigationTo('/')">HOME</p>
                     <p class="nav-link" @click="navigationTo('/about')">ABOUT US</p>
                     <p class="nav-link" @click="navigationTo('/product')">PRODUCT</p>
-               </div>
-               <profile></profile>
-               <cart></cart>
+                </div>
+                <div class="profile-card">
+                    <profile></profile> 
+                    <cart></cart>
+                </div>
      </v-app-bar>
 </template>
 
@@ -32,6 +34,12 @@ function navigationTo(where) {
   gap: 32px;
 }
 
+.profile-card {
+  display: flex;
+  align-items: center;
+  justify-content: end;
+}
+
 .nav-link {
   cursor: pointer;
   margin-left: 16px;
@@ -44,5 +52,26 @@ function navigationTo(where) {
 .nav-link:hover {
   text-decoration: none;
   color: #5c5c5c;
+}
+
+@media (max-width: 600px) {
+  .app-bar-content {
+    align-items: center; 
+  }
+
+  .profile-card {
+    margin-left: auto;
+    flex-direction: row;
+    align-items: center;
+  }
+
+  .nav-link {
+    font-size: small; /* Reduce font size */
+    margin-left: 0; /* Remove margin */
+  }
+
+  .company-name {
+    font-size: medium; /* Adjust company name size */
+  }
 }
 </style>
