e5db0733ac7914a63aedc8fb4f72100d201ecd0b
yiliang <yapyiliangww321@gmail.com>
2025-01-24 22:36:07 +0800

    set mobile view

--- src/views/header/header.vue
+++ src/views/header/header.vue
@@ -1,16 +1,18 @@
 <template>
-     <v-app-bar app dense>
-                <div class="app-bar-content">
-                    <p class="company-name" @click="navigationTo('/')">Company Name</p>
-                    <p class="nav-link" @click="navigationTo('/')">HOME</p>
-                    <p class="nav-link" @click="navigationTo('/about')">ABOUT US</p>
-                    <p class="nav-link" @click="navigationTo('/product')">PRODUCT</p>
-                </div>
-                <div class="profile-card">
-                    <profile></profile> 
-                    <cart></cart>
-                </div>
-     </v-app-bar>
+  <v-app-bar app dense>
+    <div class="app-bar-content">
+      <p class="company-name" @click="navigationTo('/')">Company Name</p>
+      <div class="nav-links-container">
+        <p class="nav-link" @click="navigationTo('/')">HOME</p>
+        <p class="nav-link" @click="navigationTo('/about')">ABOUT US</p>
+        <p class="nav-link" @click="navigationTo('/product')">PRODUCT</p>
+      </div>
+    </div>
+    <div class="profile-card">
+      <profile></profile> 
+      <cart></cart>
+    </div>
+  </v-app-bar>
 </template>
 
 <script setup>
@@ -28,50 +30,58 @@ function navigationTo(where) {
 <style>
 .app-bar-content {
   display: flex;
-  align-items: center; 
+  align-items: center;
   justify-content: center;
-  width: 100%;
+  flex: 1;
   gap: 32px;
+}
+
+.company-name {
+  cursor: pointer;
+}
+
+.nav-links-container {
+  display: flex;
+  align-items: center;
+  gap: 24px;
+}
+
+.nav-link {
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 1rem;
+  font-weight: bold;
+  color: black;
+}
+
+.nav-link:hover {
+  color: #5c5c5c;
 }
 
 .profile-card {
   display: flex;
   align-items: center;
-  justify-content: end;
-}
-
-.nav-link {
-  cursor: pointer;
-  margin-left: 16px;
-  color: black; 
-  text-decoration: none;
-  font-size: medium;
-  font-weight: bold;
-}
-
-.nav-link:hover {
-  text-decoration: none;
-  color: #5c5c5c;
+  justify-content: flex-end;
+  gap: 16px;
 }
 
 @media (max-width: 600px) {
   .app-bar-content {
+    display: inline;
+    flex-direction: column;
     align-items: center; 
   }
 
-  .profile-card {
-    margin-left: auto;
-    flex-direction: row;
-    align-items: center;
-  }
-
-  .nav-link {
-    font-size: small; /* Reduce font size */
-    margin-left: 0; /* Remove margin */
-  }
-
   .company-name {
-    font-size: medium; /* Adjust company name size */
+    text-align: center;
+  }
+
+  .nav-links-container {
+    justify-content: center;
+  }
+
+  .profile-card {
+    justify-content: center;
   }
 }
 </style>
