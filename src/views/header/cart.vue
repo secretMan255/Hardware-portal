6fa2a2f8346f0289f70bdead146d5fe89a8d8ded
yiliang <yapyiliangww321@gmail.com>
2025-01-24 22:36:29 +0800

    setup cart layout

--- src/views/header/cart.vue
+++ src/views/header/cart.vue
@@ -2,19 +2,47 @@
      <v-menu
         min-width="200px"
         rounded
-      >
-        <template v-slot:activator="{ props }">
-          <v-btn
-            icon
-            v-bind="props"
-          >
-          <v-avatar icon="mdi-cart-outline"></v-avatar>
-          </v-btn>
-        </template>
-        <v-card :width="loginStatus ? '700' : '500'">
-               <div v-if="loginStatus" class="mx-auto text-center">
-                     <v-cad-text>
-                         {{ cart }}
+     >
+          <template v-slot:activator="{ props }">
+               <v-btn icon v-bind="props">
+                    <v-badge :content="itemCount">
+                         <v-avatar icon="mdi-cart-outline"></v-avatar>
+                    </v-badge>
+               </v-btn>
+          </template>
+          <v-card :width="loginStatus ? '550' : '500'" class="scrollable-menu" @click.stop>
+               <v-card-text>
+                    <div v-if="loginStatus" class="mx-auto text-center">
+                         <v-row dense v-if="cart.length > 0">
+                              <v-col v-for="(item, index) in cart" :key="index" cols="12" md="12" class="d-flex align-center justify-space-between">
+                                   <v-img :aspect-ratio="1" max-height="50" max-width="100" :src="getProductImage(item.name)"></v-img>  
+                                   <span class="text-subtitle-1">{{ item.name }}</span> 
+                                   <div class="">
+                                        <v-icon class="mr-5" @click.stop="minusItem(item.name)" >mdi-minus</v-icon>
+                                        <span>{{ item.qty }}</span>
+                                        <v-icon class="ml-5" @click.stop="plusItem(item.name)">mdi-plus</v-icon>
+                                   </div>
+                              </v-col>
+                         </v-row>
+                         <v-row v-else="cart.length <= 0" class="text-center d-flex align-center justify-center">
+                              <v-empty-state
+                                   image="https://vuetifyjs.b-cdn.net/docs/images/components/v-empty-state/astro-dog.svg"
+                                   size="100"
+                                   text-width="250"
+                              >
+                                   <template v-slot:media>
+                                        <v-img class="mb-8"></v-img>
+                                   </template>
+
+                                   <template v-slot:title>
+                                        <div class="text-h6 text-high-emphasis">Empty in cart</div>
+                                   </template>
+
+                                   <template v-slot:text>
+                                        <div class="text-body-1">Add some item here</div>
+                                   </template>
+                              </v-empty-state>
+                         </v-row>
                          <v-divider class="my-3"></v-divider>
                          <v-btn
                               variant="text"
@@ -22,24 +50,23 @@
                          >
                               CHECKOUT
                          </v-btn>
-                    </v-cad-text>
-               </div>
-               <div v-else class="mx-auto text-center">
-                    <v-card-text>
-                         <p class="text-caption mt-1">
-                              You are not logged in.
-                         </p>
-                         <v-btn
-                              variant="text"
-                              rounded
-                              @click="login"
-                         >
-                              LOGIN
-                         </v-btn>
-                    </v-card-text>
-               </div>
-        </v-card>
-      </v-menu>
+                    </div>
+                    <div v-else class="mx-auto text-center">
+                         
+                              <p class="text-caption mt-1">
+                                   You are not logged in.
+                              </p>
+                              <v-btn
+                                   variant="text"
+                                   rounded
+                                   @click="login"
+                              >
+                                   LOGIN
+                              </v-btn>
+                    </div>
+               </v-card-text>
+          </v-card>
+     </v-menu>
 </template>
 
 <script>
@@ -49,15 +76,47 @@ export default {
      data: () => ({
           loginStatus: false,
           user: null,
-          cart: []
+          cart: [],
      }),
+     computed: {
+          itemCount() {
+               return this.cart.reduce((total, item) => total + item.qty, 0);
+          }
+     },
      methods: {
+          minusItem(itemName){
+               const itemIndex = this.cart.findIndex(cartItem => cartItem.name === itemName)
+
+               if (itemIndex !== -1) {
+                    const qty = this.cart[itemIndex].qty
+                    
+                    if (qty === 1) {
+                         this.cart.splice(itemIndex, 1)
+                    } else {
+                         this.cart[itemIndex].qty -= 1
+                    }
+
+                    sessionStorage.setItem('cart' , JSON.stringify(this.cart))
+               }
+          },
+          plusItem(itemName){
+               const itemIndex = this.cart.findIndex(cartItem => cartItem.name === itemName)
+
+               if (itemIndex !== -1) {
+                    this.cart[itemIndex].qty += 1
+
+                    sessionStorage.setItem('cart' , JSON.stringify(this.cart))
+               }
+          },
+          getProductImage(productName) {
+               console.log('product: ' , productName)
+               return new URL(`https://storage.googleapis.com/veryhardware/${productName}.jpeg`).href
+          },
           handleUserLoggedIn(user) {
                if (user) {
                     this.user = user
                     this.loginStatus = true
-               } 
-               console.log('getUser: ', this.user)
+               }               
           },
           handleUserLoggedOut(user) {
                this.user = null
@@ -70,8 +129,6 @@ export default {
                } else {
                     this.cart.push({ name: item.name, qty: 1 })
                }
-
-               sessionStorage.removeItem('cart')
                sessionStorage.setItem('cart', JSON.stringify(this.cart))
           },
           checkLogin() {
@@ -83,6 +140,15 @@ export default {
                     this.loginStatus = true
                } 
                console.log('getUserIdSession: ' , getUserIdSession)
+
+               const itemSession = sessionStorage.getItem('cart')
+               if (itemSession) {
+                    const jsonCart = JSON.parse(itemSession)
+                    this.cart = []
+                    jsonCart.forEach(item => {
+                         this.cart.push({ name: item.name, qty: item.qty })
+                    })
+               }
           },
           login() {
 
@@ -101,3 +167,13 @@ export default {
      }
 }
 </script>
+
+<style scoped>
+.scrollable-menu {
+     overflow-y: auto;
+}
+.scrollable-menu {
+     max-height: 400px;
+     overflow-y: auto;
+}
+</style>
