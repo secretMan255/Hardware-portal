ca1b9d99c0c208cb254951dfeb6cca65cf66f5a1
yiliang <yapyiliangww321@gmail.com>
2025-01-29 19:44:57 +0800

    add whatsapp avatar and remove contact us component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <v-app>
     <HeaderView />
-    
     <v-main>
-      
       <router-view />
-      
+      <WhatsApp />
     </v-main>
     <FooterView v-if="showFooter"></FooterView>
+    
   </v-app>
 </template>
 
 <script setup>
 import HeaderView from './views/header/header.vue'
 import FooterView from './views/footer/footer.vue'
+import WhatsApp from './views/whatsapp/whatsapp.vue'
 import { useRoute } from 'vue-router';
 
 const route = useRoute()
---
+++ src/views/whatsapp/whatsapp.vue
@@ -0,0 +1,24 @@
+<template>
+     <a href="https://wa.me/60172223915" target="_blank" rel="noopener noreferrer">
+          <v-icon size="50" icon="mdi-whatsapp" class="whatsapp-position"></v-icon>
+     </a>
+</template>
+
+<script>
+</script>
+
+<style>
+.avatar {
+     
+}
+
+.whatsapp-position {
+     position: fixed; 
+     bottom: 100px; 
+     right: 10px; 
+     z-index: 1; 
+     color: green;
+     background-color: white;
+     border-radius: 50%;
+}
+</style>
