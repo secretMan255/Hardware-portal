7aa8055d82ec75f116e5238e233ab6ff912245c9
yiliang <yapyiliangww321@gmail.com>
2025-01-30 20:39:06 +0800

    display remain stock and implement checkout function

--- src/views/header/cart.vue
+++ src/views/header/cart.vue
@@ -15,10 +15,13 @@
                     <div v-if="loginStatus" class="mx-auto text-center">
                          <v-row dense v-if="cart.length > 0">
                               <v-col v-for="(item, index) in cart" :key="index" cols="12" md="12" class="d-flex align-center justify-space-between mb-1">
-                                   <v-img :aspect-ratio="1" max-height="50" max-width="100" :src="getProductImage(item.name)"></v-img>  
-                                   <span class="text-subtitle-1">{{ item.name }}</span> 
+                                   <v-img :aspect-ratio="1" max-height="50" max-width="100" :src="getProductImage(item.name)"></v-img> 
+                                        <div class="d-flex flex-column"> 
+                                             <span class="text-subtitle-1">{{ item.name }}</span> 
+                                             <span class="text-grey"> Stock remain: {{ item.stock_remain }} </span>
+                                        </div>
                                         <v-icon @click.stop="minusItem(item.name)" >mdi-minus</v-icon>
-                                        <span>{{ item.qty }}</span>
+                                        <span> {{ item.qty }} </span>
                                         <v-icon @click.stop="plusItem(item.name)">mdi-plus</v-icon>
                                    <v-icon @click.stop="removeItem(item.name)">mdi-close</v-icon>
                               </v-col>
@@ -46,6 +49,8 @@
                          <v-btn
                               variant="text"
                               rounded
+                              @click="checkOut"
+                              :disabled="cart.length <= 0"
                          >
                               CHECKOUT
                          </v-btn>
@@ -62,6 +67,25 @@
                </v-card-text>
           </v-card>
      </v-menu>
+
+     <v-snackbar
+          v-model="snackbar"
+          vertical
+     >
+          <div class="text-subtitle-1 pb-2">Cart</div>
+
+          <p>{{ snackBarMsg }}</p>
+
+          <template v-slot:actions>
+          <v-btn
+               color="indigo"
+               variant="text"
+               @click="snackbar = false"
+          >
+               Close
+          </v-btn>
+          </template>
+     </v-snackbar>
 </template>
 
 <script>
@@ -74,35 +98,36 @@ export default {
           loginStatus: false,
           user: null,
           cart: [],
+          snackbar: false,
+          snackBarMsg: ''
      }),
      computed: {
           itemCount() {
-               return this.cart.reduce((total, item) => total + item.qty, 0);
+               return this.cart.length;
           }
      },
      methods: {
           async minusItem(itemName){
                const itemIndex = this.cart.findIndex(cartItem => cartItem.name === itemName)
 
-               if (itemIndex !== -1) {
-                    const qty = this.cart[itemIndex].qty
-                    const itemId = this.cart[itemIndex].id
-                    
-                    if (qty === 1) {
-                         this.cart.splice(itemIndex, 1)
-                    } else {
-                         this.cart[itemIndex].qty -= 1
-                    }
+               if (itemIndex !== -1 && this.loginStatus) {
+
                     const data = {
                          userId: this.user,
-                         itemId: itemId,
+                         itemId: this.cart[itemIndex].id,
                          recaptchaToken: await executeRecaptcha('email_login')
                     }
 
                     try {
                          const res = await CallApi.minusItemFromCart(data)
                          
-                         if (res.ret == 0 && res.data?.length > 0) {
+                         if (res.ret == 0) {
+                              if (res.data?.[0]?.msg) {
+                                   this.snackbar = true
+                                   this.snackBarMsg = `${itemName} is out of stock, please contact us.`
+                                   return 
+                              }
+
                               this.cart = res.data
                               sessionStorage.setItem('cart', JSON.stringify(this.cart))
                          }
@@ -115,7 +140,6 @@ export default {
                const itemIndex = this.cart.findIndex(cartItem => cartItem.name === itemName)
 
                if (itemIndex !== -1 && this.loginStatus) {
-                    this.cart[itemIndex].qty += 1
                    
                     const data = {
                          userId: this.user,
@@ -125,8 +149,14 @@ export default {
 
                     try {
                          const res = await CallApi.addItemToCart(data)
-                         
-                         if (res.ret == 0 && res.data?.length > 0) {
+
+                         if (res.ret == 0) {
+                              if (res.data?.[0]?.msg) {
+                                   this.snackbar = true
+                                   this.snackBarMsg = `${itemName} ${res.data[0].msg}`
+                                   return 
+                              }
+
                               this.cart = res.data
                               sessionStorage.setItem('cart', JSON.stringify(this.cart))
                          }
@@ -163,14 +193,6 @@ export default {
                return new URL(`https://storage.googleapis.com/veryhardware/${productName}.jpeg`).href
           },
           async addItemToCart(item) {
-               const existingItemIndex = this.cart.findIndex(cartItem => cartItem.name === item.name)
-               
-               if (existingItemIndex !== -1 && this.loginStatus) {
-                    this.cart[existingItemIndex].qty += 1
-               } else if (this.loginStatus) {
-                    this.cart.push({ id: item.id, name: item.name, qty: 1 })
-               }
-
                const data = {
                     userId: this.user,
                     itemId: item.id,
@@ -179,8 +201,14 @@ export default {
 
                try {
                     const res = await CallApi.addItemToCart(data)
-                    
-                    if (res.ret == 0 && res.data?.length > 0) {
+
+                    if (res.ret == 0) {
+                         if (res.data?.[0]?.msg) {
+                              this.snackbar = true
+                              this.snackBarMsg = `${item.name} ${res.data[0].msg}`
+                              return 
+                         }
+
                          this.cart = res.data
                          sessionStorage.setItem('cart', JSON.stringify(this.cart))
                     }
@@ -229,8 +257,37 @@ export default {
                     this.cart = []
                } 
           },
-          login() {
+          async checkOut(){
+               const userInfor = sessionStorage.getItem('user')
+               
+               if (userInfor.phone) {
+                    this.snackbar = true
+                    this.snackBarMsg = 'Please update your profile with a phone number before proceeding to checkout.'
+                    return
+               } else if (userInfor.address) {
+                    this.snackbar = true
+                    this.snackBarMsg = 'Please update your profile with a address before proceeding to checkout.'
+                    return
+               } else if (userInfor.postCode) {
+                    this.snackbar = true
+                    this.snackBarMsg = 'Please update your profile with a postcode before proceeding to checkout.'
+                    return
+               } else if (userInfor.city) {
+                    this.snackbar = true
+                    this.snackBarMsg = 'Please update your profile with a city before proceeding to checkout.'
+                    return
+               } else if (userInfor.country) {
+                    this.snackbar = true
+                    this.snackBarMsg = 'Please update your profile with a country before proceeding to checkout.'
+                    return
+               }
 
+               const data = {
+                    userId: this.user,
+                    recaptchaToken: await executeRecaptcha('email_login')
+               }
+
+               const res = await CallApi.checkout(data)
           }
      },
      mounted() {
