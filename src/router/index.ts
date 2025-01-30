6d06565acab5819f15bf0cff6ac5dbb4098a273c
yiliang <yapyiliangww321@gmail.com>
2025-01-30 23:54:34 +0800

    remove useless page

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from "vue-router";
 import home from "@/views/home/home.vue";
 import aboutUs from "@/views/aboutUs/aboutUs.vue";
 import products from "@/views/products/products.vue";
-import contact from "@/views/contact/contact.vue";
 
 const routes = [
   {
