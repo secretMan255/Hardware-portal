3f6db6e651e71c5679421b06936cbabf942c2a17
yiliang <yapyiliangww321@gmail.com>
2025-02-03 22:35:41 +0800

    amplifier image size component

---
+++ src/views/imageAmplifier/imageAmplifier.vue
@@ -0,0 +1,27 @@
+<template>
+     <v-dialog :model-value="modelValue" @update:model-value="updateDialog" width="400">
+          <v-img
+               :src="this.imgUrl"
+          ></v-img>
+     </v-dialog>
+</template>
+
+<script>
+export default {
+     props: {
+          modelValue: {
+               type: Boolean,
+               required: true          
+          },
+          imgUrl: {
+               type: String,
+               required: true
+          }
+     },
+     methods: {
+          updateDialog(newValue) {
+               this.$emit("update:modelValue", newValue)
+          },
+     }
+}
+</script>
