bbb05ca72873ae0803b45d9091c055c5b2698e6b
yiliang <yapyiliangww321@gmail.com>
2025-02-07 11:50:12 +0800

    remove pinia

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -5,16 +5,12 @@
  */
 
 // Plugins
-import vuetify from './vuetify'
-import pinia from '../stores'
-import router from '../router'
+import vuetify from "./vuetify";
+import router from "../router";
 
 // Types
-import type { App } from 'vue'
+import type { App } from "vue";
 
-export function registerPlugins (app: App) {
-  app
-    .use(vuetify)
-    .use(router)
-    .use(pinia)
+export function registerPlugins(app: App) {
+  app.use(vuetify).use(router);
 }
